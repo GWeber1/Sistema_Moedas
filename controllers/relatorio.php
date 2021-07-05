08923dfebdcc38d3bf26ad8b601649f9c42def35
GWeber1 <53956475+GWeber1@users.noreply.github.com>
2021-07-05 17:17:52 -0300

    Controlador de dados do relatório

---
+++ controllers/relatorio.php
@@ -0,0 +1,14 @@
+<?
+include('../vendor/autoload.php');
+require("../classes/resumo.php");
+
+ini_set('default_charset', 'UTF-8');
+$smarty = new Smarty();
+$smarty->template_dir = "templates";
+$smarty->compile_dir  = "templates_c";
+$smarty->config_dir = "configs";
+$moedas = new Moedas();
+$showMoedas = $moedas->mostraMoedas();
+$smarty->assign("moedas", $showMoedas);
+$smarty->display('../views/relatorio.html');
+?>
